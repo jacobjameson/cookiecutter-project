20f22083f213ab4ca53c74b5a88ad3397f36f1f1
jacobjameson <jacobjameson@g.harvard.edu>
2023-10-06 19:59:06 -0400

    trying again

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# cookiecutter-project
+# Cookiecutter Research Project
+
+Cookiecutter is a project to automate the generation of folder structures.
+For example, starting a new research project.
+
+## Quickstart
+
+Install the latest Cookiecutter if you haven't installed it yet (this requires
+Cookiecutter 1.4.0 or higher):
+
+```bash
+pip install -U cookiecutter
+```
+
+Or, if you use `conda`:
+
+```bash
+pip install -U cookiecutter
+```
+
+Or, if you use `brew`:
+
+```bash
+brew install cookiecutter
+```
+
+Then, generate a Research Project Template by entering the following in your command line (Powershell or Terminal):
+
+```bash
+cookiecutter gh:jacobjameson/cookiecutter-research-project
+```
+
+You will be prompted for the following values:
+
+
+Input Value        | Description
+-------------------|----------------------------------------------------------------------------------------------------------------------------------------
+`paper_title`      | Title of the paper. Example: "Academic Research Project"
+`project_acronym`  | Acronym for the project, used as a short-hand for reference. Default: "ARP"
+`full_name`        | Your full name, added to the Latex file. Other authors will have to be manually added to the Latex. Default: "Jacob Jameson"
+`email`            | Your email address, added to the Latex file. Other authors will have to be manually added to the Latex. Default: "jacobjameson@g.harvard.edu"
+`author_lastnames` | List of last names, used to name the folder of the project (separated by commas or spaces, can be ignored if `project_folder` is manually entered below). Default: "Jameson,Coots"
+`project_folder`   | Folder to create the project in. Default: `project_acronym`_`author_lastnames`, e.g. "ARP_Jameson_Coots"
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import os
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+ROOT_DIR_PATTERN = '$((ROOTDIR))'
+
+
+if __name__ == '__main__':
+    for root, dirs, files in os.walk(PROJECT_DIRECTORY):
+        for filename in files:
+            fullpath = os.path.join(root, filename)
+
+            content = None
+
+            for encoding in ('utf-8', 'latin-1'):
+                try:
+                    with open(os.path.join(root, filename), 'r', encoding=encoding) as fh:
+                        content = fh.read()
+                except UnicodeDecodeError:
+                    pass
+
+            if content is None or ROOT_DIR_PATTERN not in content:
+                continue
+
+            with open(fullpath, 'w', encoding=encoding) as fh:
+                fh.write(content.replace(ROOT_DIR_PATTERN, PROJECT_DIRECTORY))
+
+
+help = """
+                          Jgy__
+                            jWW  `""9Wf
+                          _#WWW     IW
+                         jWWWWW     IW
+                 __,yyyyyWWWWW     IWyyyy___
+            _jyWWP"^``"`.C"9*,J _.mqD:^^"WWWWWWQg__
+          jgW"^.C/"    .C'     I    `D.     'D._"WQg_
+        jWP` .C"       C'      I     `D._     `D._ "Qg_
+      jQP`  .C    ,d^^b._      I      _.d^^b.   `D._ "Qg
+     jQ^  .C"   /`   .+" \     I     / "+.   `\   `D.  XQ
+    jQ'  .C'   |`  ."    )    _I    (     ".  |    `D.  4#
+    Qf  .C     (   (    /    / /\    \     )  )     `D.  Qg
+   jW   C'      \__\_.+'    / /  \    `+._/__/       `D  jQ
+   Qf   C         C        /_/    \         D         D   Qk
+   Qf   C      _  C        \_\____/         D  _      D   QF
+   QL   C      \`+.__     _______     ______.+'/      D   QF
+   B&   C.      \   \|    ||     |    ||      /       D   Qf
+   jQ   `C.      \   |____|/     |____|/__   /      .D'   jW
+    TQ   `C.      \._   `+.__________/___/|_/      .D'   jQ`
+     9Q   `C.      C.`+._           |   |/.D'     .D'   jQ'
+      "Qg  `C.     `C.   `"+-------'   ' .D'     .D'   pW`
+       ^WQy `C.     `C.        I        .D'    _.D' yW"
+         ^9Qy_`C.    `C.       I       .D'   _.D jgW"
+            `9WQgC.__ `C.      I      .D'  _.Dp@@"`
+           ilmk `""9WQQggyyyyyygyyyyyQggQWQH""
+    
+Don't forget to sync to GitHub. Have fun!
+"""
+print(help)
