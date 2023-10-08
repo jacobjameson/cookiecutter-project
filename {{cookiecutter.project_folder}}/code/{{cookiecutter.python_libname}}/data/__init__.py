777acacdaa69046a53f44336d0753ab72c6ec10e
jacobjameson <jacobjameson@g.harvard.edu>
2023-10-08 09:44:13 -0400

    fixing folders

--- {{cookiecutter.project_folder}}/code/{{cookiecutter.python_libname}}/data/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Data module for importing and cleaning data sets.
-
-Import / download / cache datasets in `download` module.
-Get and filter sample in `sample` module.
-"""
--- {{cookiecutter.project_folder}}/code/{{cookiecutter.python_libname}}/process/__init__.py
+++
@@ -1,8 +0,0 @@
-# STDlib imports
-# import os
-
-# 3rd party imports
-# from reslib import config as __config
-
-# current module imports
-# from {{ cookiecutter.python_libname }} import config
